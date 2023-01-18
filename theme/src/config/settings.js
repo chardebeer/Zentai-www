96feff61837bcb8661fdf80c7843dd00ea80002e
Charlize de Beer <charlize@moderndaystrategy.com>
2023-01-18 10:34:01 +0200

    Updates

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,8 +1,8 @@
 export default {
-	title: `Odyssey Astro Theme | A Marketing Website Theme for Startups and Businesses`,
-	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
-	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
-	name: `Odyssey`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	title: `Zentai Systems | Transforming Your Ideas Into Results `,
+	description: `"Welcome to Zentai Systems, your partner in digital success. Our team of experienced professionals specializes in website design, UX design, UI design, digital marketing, event planning, event management, software development, social media marketing, and graphic design. We are experts in Shopify development and can help your business achieve its online goals. From website creation and e-commerce development to search engine optimization and online advertising, we offer a range of services to help your business thrive. Contact us today to learn more and see how we can help transform your ideas into results`,
+	url: `https://zentaisystems.com`, // No trailing slash!
+	name: `Zentai Systems`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: true,
   showLittleSticksPlug: true, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
 };
